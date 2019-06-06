2ce00cdf9d16b2a55c8e29da3c514fee1275fca3
intelligide <intelligide@hotmail.fr>
2019-06-06 08:58:53 +0200

    Add cpp sources

---
+++ Assets/Scripts/NativeFunction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class NativeFunction : MonoBehaviour
+{
+
+    [DllImport("Hello")]
+    private static extern int ReturnNumber();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(ReturnNumber());
+    }
+}
---
+++ cpp/hello.cpp
@@ -0,0 +1,9 @@
+#include <string>
+
+extern "C" 
+{
+    int ReturnNumber()
+    {
+        return 4587;
+    }
+};
